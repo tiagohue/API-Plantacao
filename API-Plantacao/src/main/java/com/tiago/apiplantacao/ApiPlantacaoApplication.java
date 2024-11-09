6abde61f706a3cd60f6dc962d87eacaaea2c3065
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-11-09 16:46:16 -0300

    Criacao da API

---
+++ API-Plantacao/src/main/java/com/tiago/apiplantacao/ApiPlantacaoApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.apiplantacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPlantacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPlantacaoApplication.class, args);
+	}
+
+}
---
+++ API-Plantacao/src/test/java/com/tiago/apiplantacao/ApiPlantacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.apiplantacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPlantacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
