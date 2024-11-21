3f35aec34772863d5e7b53e80956fb6410183c8a
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-11-21 19:17:01 -0300

    Criação do metodo get para todos

--- API-Plantacao/src/main/java/com/tiago/apiplantacao/controller/CanteiroController.java
+++ API-Plantacao/src/main/java/com/tiago/apiplantacao/controller/CanteiroController.java
@@ -1,10 +1,25 @@
 package com.tiago.apiplantacao.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tiago.apiplantacao.model.Canteiro;
+import com.tiago.apiplantacao.repository.CanteiroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/canteiros")
 public class CanteiroController {
+
+    @Autowired
+    private CanteiroRepository canteiroRepository;
+
+    @GetMapping
+    public List<Canteiro> getAllCanteiros() {
+        return canteiroRepository.findAll();
+    }
 
 }
