1b04a66e951e7fbf90798cbdc87b4cbad8a7e6af
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-11-21 18:24:08 -0300

    Criação dos controllers

---
+++ API-Plantacao/src/main/java/com/tiago/apiplantacao/controller/CanteiroController.java
@@ -0,0 +1,10 @@
+package com.tiago.apiplantacao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/canteiros")
+public class CanteiroController {
+
+}
