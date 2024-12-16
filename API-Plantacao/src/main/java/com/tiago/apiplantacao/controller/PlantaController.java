d080dd420fe5603090a48ddc7b72ed5df1acc471
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-12-16 10:11:15 -0300

    Criação do Controller de Planta

--- API-Plantacao/src/main/java/com/tiago/apiplantacao/controller/PlantaController.java
+++ API-Plantacao/src/main/java/com/tiago/apiplantacao/controller/PlantaController.java
@@ -1,10 +1,52 @@
 package com.tiago.apiplantacao.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tiago.apiplantacao.dto.request.PlantaRequestDTO;
+import com.tiago.apiplantacao.dto.response.PlantaResponseDTO;
+import com.tiago.apiplantacao.service.PlantaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/plantas")
+@RequiredArgsConstructor
 public class PlantaController {
 
+    private final PlantaService plantaService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<PlantaResponseDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(plantaService.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PlantaResponseDTO>> findAll() {
+        return ResponseEntity.ok().body(plantaService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<PlantaResponseDTO> register(
+            @RequestBody PlantaRequestDTO plantaRequestDTO,
+            UriComponentsBuilder uriBuilder) {
+
+        PlantaResponseDTO plantaResponseDTO = plantaService.register(plantaRequestDTO);
+        URI uri = uriBuilder.path("/plantas/{id}").buildAndExpand(plantaResponseDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(plantaResponseDTO);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<PlantaResponseDTO> update(@RequestBody PlantaRequestDTO plantaDTO,
+                                                       @PathVariable Long id) {
+        return ResponseEntity.ok().body(plantaService.update(plantaDTO, id));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        return ResponseEntity.ok().body(plantaService.delete(id));
+    }
 }
