95eed8085181671082646896905ddba118fb5514
Tiago Freitas <tiagohuehuehue@gmail.com>
2024-12-16 10:42:39 -0300

    Criação do Service de Responsavel

---
+++ API-Plantacao/src/main/java/com/tiago/apiplantacao/service/ResponsavelService.java
@@ -0,0 +1,18 @@
+package com.tiago.apiplantacao.service;
+
+import com.tiago.apiplantacao.dto.request.ResponsavelRequestDTO;
+import com.tiago.apiplantacao.dto.response.ResponsavelResponseDTO;
+
+import java.util.List;
+
+public interface ResponsavelService {
+    ResponsavelResponseDTO findById(Long id);
+
+    List<ResponsavelResponseDTO> findAll();
+
+    ResponsavelResponseDTO register(ResponsavelRequestDTO canteiroDTO);
+
+    ResponsavelResponseDTO update(ResponsavelRequestDTO canteiroDTO, Long id);
+
+    String delete(Long id);
+}
